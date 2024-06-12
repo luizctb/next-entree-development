33667f13403099e768c82e808ed77ab61ee0fc9e
Luiz Tome <luizcarlostb@hotmail.com>
2024-06-12 09:18:33 -0300

    feat: card list adress

--- src/app/fakeDatas/FakeList.tsx
+++ src/app/fakeDatas/FakeList.tsx
@@ -1,21 +1,35 @@
 export const information03 = [ 
 
    {
-      title: "Endereço",
+      title: "Endereço: Fazenda Nova",
       text: "Descrição básica do local e do projeto, para facilitar a identificação",
       image: 'http://localhost:3000/fazenda01.png',
       text1: "BUTTON LARGE"
    },
 
    {
-      title: "Endereço",
+      title: "Endereço: Fazenda do Rancho",
       text: "Descrição básica do local e do projeto, para facilitar a identificação",
       image: 'http://localhost:3000/fazenda01.png',
       text1: "BUTTON LARGE"
    },
 
    {
-      title: "Endereço",
+      title: "Endereço: Fazenda Vale do Sol",
+      text: "Descrição básica do local e do projeto, para facilitar a identificação",
+      image: 'http://localhost:3000/fazenda01.png',
+      text1: "BUTTON LARGE"
+   },
+
+   {
+      title: "Endereço: Fazenda Canta Galo",
+      text: "Descrição básica do local e do projeto, para facilitar a identificação",
+      image: 'http://localhost:3000/fazenda01.png',
+      text1: "BUTTON LARGE"
+   },
+
+   {
+      title: "Endereço: Fazenda do Norte",
       text: "Descrição básica do local e do projeto, para facilitar a identificação",
       image: 'http://localhost:3000/fazenda01.png',
       text1: "BUTTON LARGE"
